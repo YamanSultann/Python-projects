b5767974c91efba558419da7259953c6338ea64d
YamanSultann <yamansultanlabib@gmail.com>
2024-09-16 15:36:35 +0600

    initial commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,4 +8,16 @@ if user_action == computer_action:
     print("It's a TIE, no one wins")
 elif user_action == "rock":
     if computer_action == "paper":
-        
+        print("Paper beats rock, you LOSE")
+    elif computer_action == "scissor":
+        print("Rock beats scissor, you WIN")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper beats rock, you WIN")
+    elif computer_action == "scissor":
+        print("Scissor beats paper, you LOSE")
+elif user_action == "scissor":
+    if computer_action == "rock":
+        print("Rock beats scissor, you LOSE")
+    elif computer_action == "paper":
+        print("Scissor beats paper, you WIN")

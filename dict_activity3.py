6a57dd4e9e98b90a4eed207618ba1c6cb13821d6
YamanSultann <yamansultanlabib@gmail.com>
2024-09-30 15:26:34 +0600

    initial commit

---
+++ dict_activity3.py
@@ -0,0 +1,8 @@
+country_code = {'India': '0091',
+                'Australia': '0061',
+                'Nepal': '00977'}
+print("The country code for India is- ")
+print(country_code.get('India'),'Not found')
+
+print("The country code for Japan is- ")
+print(country_code.get('japan'),'Not found')

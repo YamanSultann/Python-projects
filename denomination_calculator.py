c7a0d437c8d6f2aed2f07253b5efd7c69f08ec87
YamanSultann <yamansultanlabib@gmail.com>
2024-12-17 15:34:09 +0600

    initial commit

--- denomination_calculator.py
+++ denomination_calculator.py
@@ -3,67 +3,83 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 
 root = Tk()
-root.geometry("650x400")
-root.title("Denomination Calculator")
-root.configure(bg="light blue")
+root.title('Denomination Counter')
+root.configure(bg='light blue')
+root.geometry('650x400')
 
-upload = Image.open("app_img.png")
-upload = upload.resize(300 , 300)
+upload = Image.open("app_img.jpg")
+upload = upload.resize((300, 300))
 image = ImageTk.PhotoImage(upload)
-label = Label(root , image=image , bg="light blue")
-label.place(x=180 , y=20)
+label = Label(root, image=image, bg='light blue')
+label.place(x=180, y=20)
 
-label1 = Label(root , text = "Hey User! Welcome to Denomination Counter App." , bg = "light blue")
-label1.place(relx=0.5 , y=340 , anchor=CENTER)
+label1 = Label(root,
+               text="Hey User! Welcome to Denomination Counter Application.",
+               bg='light blue')
+label1.place(relx=0.5, y=340, anchor=CENTER)
 
 def msg():
-    MsgBox = messagebox.showinfo("Alert" , "Do you want to calculate the denomination count?")
-    if MsgBox == "Ok":
+    MsgBox = messagebox.showinfo(
+        "Alert", "Do you want to calculate the denomination count?")
+    if MsgBox == 'ok':
         topwin()
 
-def topwin():
-    top = Tk()
-    top.geometry("650x400")
-    top.title("Denomination Calculator")
-    top.configure(bg="light blue")
+button1 = Button(roottext="Let's get started!",command=msg,bg='brown',fg='white'button1.place(x=260, y=360))
 
-    label2 = Label(top,text="Enter total amount")
+def topwin():
+    top = Toplevel()
+    top.title("Denominations Calculator")
+    top.configure(bg='light grey')
+    top.geometry("600x350+50+50")
+    
+
+    label = Label(top, text="Enter total amount", bg='light grey')
     entry = Entry(top)
-    label3 = Label(top,text="Here are the number of notes for each denomination",bg="light blue")
-    l1 = Label(top,text="1000",bg="light grey")
-    l2 = Label(top,text="100",bg="light grey")
-    l3 = Label(top,text="10",bg="light grey")
-    l4 = Label(top,text="1",bg="light grey")
+    lbl = Label(top, text="Here are number of notes for each denomination", bg='light grey')
+
+    l1 = Label(top, text="2000", bg='light grey')
+    l2 = Label(top, text="500", bg='light grey')
+    l3 = Label(top, text="100", bg='light grey')
 
     t1 = Entry(top)
     t2 = Entry(top)
     t3 = Entry(top)
-    t4 = Entry(top)
 
     def calculator():
         try:
             global amount
             amount = int(entry.get())
-            note1000 = amount // 1000
-            amount %= 1000
+            note2000 = amount // 2000
+            amount %= 2000
+            note500 = amount // 500
+            amount %= 500
             note100 = amount // 100
-            amount %= 100
-            note10 = amount // 10
-            amount %= 10
-            note1 = amount // 1
-            amount %= 1
 
-            t1.delete(0,END)
-            t2.delete(0,END)
-            t3.delete(0,END)
-            t4.delete(0,END)
+            t1.delete(0, END)
+            t2.delete(0, END)
+            t3.delete(0, END)
 
-            t1.insert(END,str(note1000))
-            t2.insert(END,str(note100))
-            t3.insert(END,str(note10))
-            t4.insert(END,str(note1))
+            t1.insert(END, str(note2000))
+            t2.insert(END, str(note500))
+            t3.insert(END, str(note100))
         except ValueError:
-            messagebox.showerror("Error","Please enter a valid amount")
+            messagebox.showerror("Error", "Please enter a valid number.")
 
-    btn = Button(top,text="Calculate",cammond=calculate,bg="brown",fg="white")
-    
+    btn = Button(top, text='Calculate', command=calculator, bg='brown', fg='white')
+
+    label.place(x=230, y=50   )
+    entry.place(x=200, y=80   )
+    btn.place(x=240, y=120   )
+    lbl.place(x=140, y=170   )
+
+    l1.place(x=180, y=200   )
+    l2.place(x=180, y=230   )
+    l3.place(x=180, y=260   )
+
+    t1.place(x=270, y=200   )
+    t2.place(x=270, y=230   )
+    t3.place(x=270, y=260)
+
+    top.mainloop()
+
+root.mainloop()
